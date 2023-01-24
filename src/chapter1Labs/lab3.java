dddea5835880499013feeff0c83e78d394bc5475
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-01-24 13:20:19 +0100

    Exercice 1,2,3,4

---
+++ src/chapter1Labs/lab3.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package chapter1Labs;
+
+/**
+ * @author siham
+ *
+ */
+public class lab3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		EXERCICE 1 :
+//		Question 2
+//		String[] daysOfWeeks = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+		
+//		System.out.println(daysOfWeeks.length);
+
+//		Question3
+//--------------------- 1.First Method using for ---------------
+//		for(int i=0;i<daysOfWeeks.length;i++) {
+//			System.out.println(daysOfWeeks[i]);
+//		}
+		
+//---------------------- 2.Second Method using for(for-each) ---------------
+//		for(String d:daysOfWeeks) {
+//			System.out.println(d);
+//		}
+
+//		Question 4
+//		for (int i=6; 0 <=i && i < daysOfWeeks.length ; i--) {
+//			System.out.println(daysOfWeeks[i]);
+//		}
+		
+//		EXERCICE 2 :
+//		int i=0;
+//		while(i<= 20) {
+//			if (i%2 ==0)
+//				System.out.println(i);
+//			i++;
+//
+//		}
+		
+//		EXERCICE 3 :
+//		for(int i=1;i<=100;i++) {
+//			if(i>=50 && i<=60) 
+//				continue;
+//			System.out.print(i+",");
+//		}
+		
+//		EXERCICE 4 :
+//		String[] months = {"January","February","May","April","March","June","July","Aout","September","October","November","December"};	
+//		int c=1;
+//		while(c<=12) {			
+//			switch (c) {
+//			case 1:
+//			case 3:
+//			case 5:
+//			case 7:
+//			case 8:
+//			case 10:
+//			case 12:
+//				System.out.println("There are 31 days in " +months[c-1]);
+//				break;
+//			case 2:
+//				System.out.println("There are 28 days in " +months[c-1]);
+//				break;
+//			case 4:
+//			case 6:
+//			case 9:
+//			case 11:
+//				System.out.println("There are 30 days in " +months[c-1]);
+//				break;
+//			default:
+//				System.out.println("Incorrect");
+//
+//			}
+//			c++;
+//		}
+		
+// 		CHALLENGE EXERCICE :
+		String[] days= {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+		for(String d:days)
+			System.out.print(d + " ");
+		for() {
+			
+		}
+	}
+
+}
---
+++ src/lab2.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+
+/**
+ * @author siham
+ *
+ */
+public class lab2 {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("Hello World");
+//		int width;
+//		int height;
+//		int area;
+//		double radius = 10.0;
+//		double pi = 3.14;
+//		boolean result = true;
+		
+		
+		
+		
+//		width = 8;
+//		height = 12;
+//		area=96;
+//		
+//		System.out.println("The value of width is : "+width +"\nThe value of height is : "+height+"\nThe value of area is : "+area+"\nThe value of radius is : "+radius+"\nThe value of pi is : "+pi+"\nThe value of result is : "+result);
+		 
+		
+//--------------Arrays exercice ---------------
+		int[] daysInMonths = new int [12];
+		String[] monthNames = {"January","February","May","April","March","June","July","Aout","September","October","November","December"}	;	
+		daysInMonths[0] = 31;
+		daysInMonths[1] = 28;
+		daysInMonths[2] = 31;
+		daysInMonths[3] = 30;
+		daysInMonths[4] = 31;
+		daysInMonths[5] = 30;
+		daysInMonths[6] = 31;
+		daysInMonths[7] = 31;
+		daysInMonths[8] = 30;
+		daysInMonths[9] = 31;
+		daysInMonths[10] = 30;
+		daysInMonths[11] = 31;
+		
+//		daysInMonths[12] = 31;
+//		daysInMonths[13] = 31;
+
+
+//		System.out.println(monthNames[0] +" has "+daysInMonths[0]+ " days");
+//		System.out.println(monthNames[1] +" has "+daysInMonths[1]+ " days");
+//		System.out.println(monthNames[2] +" has "+daysInMonths[2]+ " days");
+//		System.out.println(monthNames[3] +" has "+daysInMonths[3]+ " days");
+//		System.out.println(monthNames[4] +" has "+daysInMonths[4]+ " days");
+//		System.out.println(monthNames[5] +" has "+daysInMonths[5]+ " days");
+//		System.out.println(monthNames[6] +" has "+daysInMonths[6]+ " days");
+//		System.out.println(monthNames[7] +" has "+daysInMonths[7]+ " days");
+//		System.out.println(monthNames[8] +" has "+daysInMonths[8]+ " days");
+//		System.out.println(monthNames[9] +" has "+daysInMonths[9]+ " days");
+//		System.out.println(monthNames[10] +" has "+daysInMonths[10]+ " days");
+//		System.out.println(monthNames[11] +" has "+daysInMonths[11]+ " days");
+
+		int a =10;
+		
+		switch(a){
+			case 20:
+				System.out.println("This is case 20");
+				break;
+			case 30:
+			case 10:
+				System.out.println("Perfect");
+				break;
+			case 40:
+				System.out.println("This is case 40");
+				break;
+			default:
+				System.out.println("This is the default case");
+				break;
+		}
+		
+		
+		
+		
+	}
+
+}
